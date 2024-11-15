26ef05d58d92627575645051394268b441d85a24
lvzhangjie <1721597041@qq.com>
2024-11-16 00:47:40 +0800

    feat:新增部分css特效

--- src/components/BusinessCard/index.jsx
+++ src/components/BusinessCard/index.jsx
@@ -5,6 +5,8 @@ export default function BusinessCard({
     avatar = "",
     title = "这是网页的标题",
     signature = "这是你的签名",
+    school = "这是你的学校",
+    grade = "这是你的年级",
     address = "这是你的地址",
     qq = "",
     wechat = "",
@@ -16,7 +18,9 @@ export default function BusinessCard({
             <div className="card">
                 <img src={avatar} alt="Avatar" className="card-avatar" />
                 <p className="card-title">{title}</p>
-                <p className="card-signature">{signature}</p>
+                <p className="card-signature">
+                    <span>{signature}</span>
+                </p>
                 <div className="divider"></div>
                 <div className="contact">
                     <a
@@ -45,16 +49,27 @@ export default function BusinessCard({
                 <div className="divider"></div>
                 <div className="info-box">
                     <span className="info-title">毕业院校</span>
-                    <span className="info-content">华中科技大学</span>
+                    <span className="info-content">{school}</span>
                 </div>
                 <div className="info-box">
                     <span className="info-title">所在城市</span>
-                    <span className="info-content">湖北 武汉</span>
+                    <span className="info-content">{address}</span>
                 </div>
                 <div className="info-box">
                     <span className="info-title">年级</span>
-                    <span className="info-content">25届毕业生</span>
+                    <span className="info-content">{grade}</span>
                 </div>
+                <div className="divider"></div>
+                <a
+                    className="contact-me"
+                    href={
+                        "Mailto:" +
+                        email +
+                        "?Subject=邮箱标题&amp;Body=邮箱内容！"
+                    }
+                >
+                    联系我
+                </a>
             </div>
         </>
     );
