470a41c87bfa9f1a00a129066de84d08bb44455a
lvzhangjie <1721597041@qq.com>
2024-11-18 15:45:19 +0800

    feat:新增联系页面标语

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,7 +3,19 @@ import React from "react";
 export default function Contact() {
     return (
         <>
-            <div>Contact</div>
+            <div className="hello-board">
+                <p>Here is my contact information！</p>
+                <p>Let's Build Tomorrow</p>
+                <p>Together</p>
+                <a className="navbtn" href="#content-board">
+                    了解一下
+                </a>
+            </div>
+            <div className="content-board" id="content-board">
+                <div className="contact-item">
+                    <span></span>
+                </div>
+            </div>
         </>
     );
 }
