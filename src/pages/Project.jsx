36db5bd99f2cb2df1c5a4003ced4e9400b690ca4
lvzhangjie <1721597041@qq.com>
2024-11-18 13:35:54 +0800

    feat:项目页面新增标语

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -3,7 +3,14 @@ import React from "react";
 export default function Project() {
     return (
         <>
-            <div>这里是Project</div>
+            <div className="hello-board">
+                <p>These are my projects!</p>
+                <p>Crafting Pixels into </p>
+                <p>Dreams</p>
+                <a className="navbtn" href="#content-board">
+                    了解一下
+                </a>
+            </div>
         </>
     );
 }
