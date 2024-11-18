2f713c91e75c2c0b38b75e3e1dff31dc5a4d706a
lvzhangjie <1721597041@qq.com>
2024-11-18 20:26:59 +0800

    feat:联系页面完善

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,6 +1,11 @@
 import React from "react";
+import "@/pages/Contact.css";
+import Divider from "@/components/Divider";
 
 export default function Contact() {
+    function sendMessage() {
+        alert("抱歉，发送功能暂未开放，敬请期待~");
+    }
     return (
         <>
             <div className="hello-board">
@@ -11,9 +16,27 @@ export default function Contact() {
                     了解一下
                 </a>
             </div>
-            <div className="content-board" id="content-board">
-                <div className="contact-item">
-                    <span></span>
+            <div className="content-board contact-board" id="content-board">
+                <div className="contact-card">
+                    <div className="card-header"></div>
+                    <span className="iconfont icon-email"></span>
+                    <p className="contact-way">email</p>
+                    <p className="contact-num">1721597041@qq.com</p>
+                </div>
+                <div className="contact-card">
+                    <div className="card-header"></div>
+                    <span className="iconfont icon-github"></span>
+                    <p className="contact-way">github</p>
+                    <p className="contact-num">hust-lzj</p>
+                </div>
+                <Divider title="联系我" num="02"></Divider>
+                <div className="email-box">
+                    <input type="text" placeholder="收件人" />
+                    <input type="text" placeholder="主题" />
+                    <textarea placeholder="邮件内容"></textarea>
+                    <button type="submit" onClick={sendMessage}>
+                        发送
+                    </button>
                 </div>
             </div>
         </>
