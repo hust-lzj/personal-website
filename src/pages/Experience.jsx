7f8aa54899c2ef72244002c8da64d7365435a94b
lvzhangjie <1721597041@qq.com>
2024-11-18 18:55:16 +0800

    feat:新增各个页面标语

--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -3,7 +3,19 @@ import React from "react";
 export default function Experience() {
     return (
         <>
-            <div>Experience</div>
+            <div className="hello-board">
+                <p>Mastered Ability！</p>
+                <p>Continuous Learning, </p>
+                <p>Continuous Growth</p>
+                <a className="navbtn" href="#content-board">
+                    了解一下
+                </a>
+            </div>
+            <div className="content-board" id="content-board">
+                <div className="contact-item">
+                    <span></span>
+                </div>
+            </div>
         </>
     );
 }
--- src/pages/Other.jsx
+++ src/pages/Other.jsx
@@ -3,7 +3,19 @@ import React from "react";
 export default function Other() {
     return (
         <>
-            <div>Other</div>
+            <div className="hello-board">
+                <p>Photos！</p>
+                <p>Life is like a box of </p>
+                <p>chocolates</p>
+                <a className="navbtn" href="#content-board">
+                    了解一下
+                </a>
+            </div>
+            <div className="content-board" id="content-board">
+                <div className="contact-item">
+                    <span></span>
+                </div>
+            </div>
         </>
     );
 }
