083611c5fe57e563937b3f0b9c00380883d30f56
lvzhangjie <1721597041@qq.com>
2024-11-18 12:18:16 +0800

    feat:当前导航底部划线高亮

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import "@/components/Navbar/Navbar.css";
 import "@/assets/icons/iconfont.css";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 export default function Navbar() {
     const logoImageUrl = new URL("@/assets/logo.png", import.meta.url).href;
+    function useActiveLink(path) {
+        const location = useLocation();
+        return location.pathname === path;
+    }
     return (
         <>
             <nav className="navbar">
@@ -18,19 +22,37 @@ export default function Navbar() {
                         <h3>个人网站</h3>
                     </div>
                     <nav className="nav-links">
-                        <li>
+                        <li className={useActiveLink("/") ? "active-link" : ""}>
                             <Link to="/">主页</Link>
                         </li>
-                        <li>
+                        <li
+                            className={
+                                useActiveLink("/project") ? "active-link" : ""
+                            }
+                        >
                             <Link to="/project">项目</Link>
                         </li>
-                        <li>
+                        <li
+                            className={
+                                useActiveLink("/experience")
+                                    ? "active-link"
+                                    : ""
+                            }
+                        >
                             <Link to="/experience">经验</Link>
                         </li>
-                        <li>
+                        <li
+                            className={
+                                useActiveLink("/contact") ? "active-link" : ""
+                            }
+                        >
                             <Link to="/contact">联系</Link>
                         </li>
-                        <li>
+                        <li
+                            className={
+                                useActiveLink("/other") ? "active-link" : ""
+                            }
+                        >
                             <Link to="/other">其他</Link>
                         </li>
                         <li className="setting">
